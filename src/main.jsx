7de0d78ee4e6ab9b7f59b7ef0cbb30a08f04c807
pfsnc <227409799+pfsnc@users.noreply.github.com>
2025-08-20 03:46:27 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'  // 添加這行
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
